81130c5e3601f1ddf0090c000010b2c7848bae27
jalanprakhar <iit2020200@iiita.ac.in>
2023-03-02 00:13:38 +0530

    Added git login to testing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:8000/" });
 export const api = {
-  login: (email, password) => API.post("/auth/login", { email, password }),
+  login: (email, password) => API.get("/auth/github"),
   signup: (email, password) => API.post("/auth/signup", { email, password }),
   getSelf: (params) => API.get("/users/user", { params }),
   updateUser: (newOb, params) =>
--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 require("./db/connection.js");
 const path = require("path");
 const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+const GitHubStrategy = require("passport-github2").Strategy;
 const bodyParser = require("body-parser");
 const authRouter = require("./routers/auth.js");
 const userRouter = require("./routers/users.js");
@@ -16,8 +19,61 @@ app.use(morgan("combined"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "bla bla bla",
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use("/auth", authRouter);
+// app.use("/auth", authRouter);
+
+passport.serializeUser(function (user, done) {
+  console.log("serializeUser", user);
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  console.log("deserializeUser", user);
+  done(null, user);
+});
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+app.get("/auth/error", (req, res) => res.send("Unknown Error"));
+app.get(
+  "/auth/github",
+  (req, res, next) => {
+    console.log(req);
+    next();
+  },
+  passport.authenticate("github", { scope: ["user:email"] })
+);
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/auth/error" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+app.get("/logout", (req, res) => {
+  req.logOut(() => {
+    console.log("Done logout");
+  });
+  res.redirect("/");
+});
+
 app.use("/users", userRouter);
 
 app.get("/*", (req, res) => {
